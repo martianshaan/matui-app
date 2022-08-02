0739a5f829942a185dfd27470b31ad6ab7e5b68e
martianshaan <shantanu.kale@viit.ac.in>
2022-08-02 19:32:54 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import LoginScreen from './components/LoginScreen';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <LoginScreen/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
